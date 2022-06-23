d39bb9f17411b584653f76d7f3706614f75865fa
aizazm7 <107006384+aizazm7@users.noreply.github.com>
2022-06-24 02:25:08 +0500

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -650,7 +650,7 @@ const Home = (props: HomeProps) => {
                 <MintContainer>
                     <DesContainer>
                         <NFT elevation={3}>
-                            <h2>Kumaneko</h2>
+                            <h2>Kumaneko (Mint 5 And Get 1 Free)</h2>
                             <br/>
                             <div><Price
                                 label={isActive && whitelistEnabled && (whitelistTokenBalance > 0) ? (whitelistPrice + " " + priceLabel) : (price + " " + priceLabel)}/><Image
